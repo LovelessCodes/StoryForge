e62abc27423182179bcace6e4a1a24cd3354a616
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-09-15 14:17:59 +0200

    feat(news): ✨ refactor news component layout and enhance card structure
    * Updated the layout of the news component for better responsiveness.
    * Replaced `ScrollArea` with a more straightforward flexbox layout.
    * Added `CardContent` and `CardFooter` for improved information display.
    * Enhanced the description display with line clamping for better readability.

--- src/routes/news.tsx
+++ src/routes/news.tsx
@@ -4,11 +4,12 @@ import { invoke } from "@tauri-apps/api/core";
 import { Loader2 } from "lucide-react";
 import {
 	Card,
+	CardContent,
 	CardDescription,
+	CardFooter,
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 interface NewsItem {
 	title: string;
@@ -47,41 +48,40 @@ function RouteComponent() {
 	}
 
 	return (
-		<div className="flex flex-col gap-6 px-4 py-2">
-			<h1 className="text-3xl font-bold mb-4">Vintage Story News</h1>
-			<div className="flex gap-2 flex-col">
-				<h2 className="text-xl font-bold mb-2">Newest releases</h2>
-				<ScrollArea className="w-[calc(100vw-10rem-32px)]">
-					<div className="flex w-max space-x-4">
-						{news && news.length > 0 ? (
-							news.map((item) => (
-								<Card
-									className="hover:shadow-lg transition-shadow w-[300px]"
-									key={item.guid}
-								>
-									<Link
-										className="block"
-										rel="noopener noreferrer"
-										target="_blank"
-										to={item.link}
-									>
-										<CardHeader>
-											<CardTitle className="text-xl mb-1">
-												{item.title}
-											</CardTitle>
-											<CardDescription className="text-xs text-muted-foreground mb-2">
-												{new Date(item.pubDate).toLocaleString()}
-											</CardDescription>
-										</CardHeader>
-									</Link>
-								</Card>
-							))
-						) : (
-							<div className="text-muted-foreground">No news found.</div>
-						)}
-					</div>
-					<ScrollBar orientation="horizontal" />
-				</ScrollArea>
+		<div className="flex gap-2 flex-col px-4 py-2 h-screen w-full grid-rows-[min-content_1fr]">
+			<h2 className="text-xl font-bold mb-2">Newest releases</h2>
+			<div className="flex flex-col w-full space-y-4 overflow-auto">
+				{news && news.length > 0 ? (
+					news.map((item) => (
+						<Card
+							className="hover:shadow-lg transition-shadow w-full"
+							key={item.guid}
+						>
+							<Link
+								className="block"
+								rel="noopener noreferrer"
+								target="_blank"
+								to={item.link}
+							>
+								<CardContent>
+									<CardHeader>
+										<CardTitle className="text-xl">{item.title}</CardTitle>
+									</CardHeader>
+									<CardDescription className="text-s text-muted-foreground mb-2 line-clamp-4">
+										{item.description.replace(/<\/?[^>]+(>|$)/g, "")}
+									</CardDescription>
+									<CardFooter className="flex justify-end">
+										<p className="text-xs text-muted-foreground">
+											{new Date(item.pubDate).toLocaleString()}
+										</p>
+									</CardFooter>
+								</CardContent>
+							</Link>
+						</Card>
+					))
+				) : (
+					<div className="text-muted-foreground">No news found.</div>
+				)}
 			</div>
 		</div>
 	);
