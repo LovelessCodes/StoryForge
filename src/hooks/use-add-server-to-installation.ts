e80b7edb676b434a148e1beccb6f83d8eff7e5e4
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-09-25 09:14:27 +0200

    feat(hooks): 🚀 add useAddServerToInstallation hook
    * Introduced a new custom hook `useAddServerToInstallation` for managing server installations.
    * Utilizes `useMutation` from `@tanstack/react-query` to handle asynchronous server addition.
    * Integrates Tauri's `invoke` function to communicate with the backend.

---
+++ src/hooks/use-add-server-to-installation.ts
@@ -0,0 +1,30 @@
+import { type UseMutationOptions, useMutation } from "@tanstack/react-query";
+import { invoke } from "@tauri-apps/api/core";
+
+export type AddServerToInstallationProps = {
+	server: string;
+	installationId: number;
+};
+
+export const useAddServerToInstallation = (
+	props?: UseMutationOptions<
+		unknown,
+		Error,
+		AddServerToInstallationProps,
+		unknown
+	>,
+) =>
+	useMutation({
+		mutationFn: ({
+			server,
+			installationId,
+		}: {
+			server: string;
+			installationId: number;
+		}) =>
+			invoke("add_server_to_installation", {
+				installationId,
+				server,
+			}),
+		...props,
+	});
