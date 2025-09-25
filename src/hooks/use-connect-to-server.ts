674f5c742951e0af11337bed367484c1d029c2ab
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-09-25 09:14:40 +0200

    feat(hooks): 🚀 enhance useConnectToServer with server validation
    - Updated the mutation options to include `name`, `pub`, and `ip` parameters.
    - Integrated `useAddServerToInstallation` and `useCheckServerInInstallation` hooks for improved server management.
    - Added logic to check if a server is already in the installation before adding it.

--- src/hooks/use-connect-to-server.ts
+++ src/hooks/use-connect-to-server.ts
@@ -2,21 +2,51 @@ import { type UseMutationOptions, useMutation } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api/core";
 import { toast } from "sonner";
 import { useInstallations } from "@/stores/installations";
+import { useAddServerToInstallation } from "./use-add-server-to-installation";
+import { useCheckServerInInstallation } from "./use-check-server-in-installation";
 
 export const useConnectToServer = (
 	props?: UseMutationOptions<
 		void,
 		Error,
-		{ ip: string; password: string; installationId: number }
+		{
+			name: string;
+			ip: string;
+			password: string;
+			installationId: number;
+			pub?: boolean;
+		}
 	>,
 ) => {
 	const { installations, updateLastPlayed } = useInstallations();
+	const { mutateAsync: addServer } = useAddServerToInstallation({
+		onError: (error) => {
+			throw error;
+		},
+	});
+	const { mutateAsync } = useCheckServerInInstallation({
+		onSuccess: async (data, variables) => {
+			if (data === false) {
+				await addServer({
+					installationId: variables.installationId,
+					server: variables.server,
+				});
+			}
+		},
+	});
 	return useMutation({
 		...props,
-		mutationFn: ({ ip, password, installationId }) =>
-			invoke("play_game", {
+		mutationFn: async ({ name, ip, password, installationId, pub }) => {
+			if (!pub) {
+				await mutateAsync({
+					installationId,
+					server: `${name},${ip},${password ? password : ""}`,
+				});
+			}
+			await invoke("play_game", {
 				options: { installation_id: installationId, password, server: ip },
-			}),
+			});
+		},
 		onError: (error) => {
 			toast.error(`Error connecting to server: ${error.message}`);
 		},
