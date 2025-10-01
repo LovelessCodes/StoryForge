5c9d777b37265825f2172a3a784074ad14a32ce2
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-10-01 13:28:13 +0200

    feat(worlds): ✨ add `useCopyToClipboard` hook for improved seed copying functionality
    * Introduced a new custom hook `useCopyToClipboard` to handle clipboard operations.
    * Enhanced `WorldItem` component to utilize the new hook, allowing users to copy world seeds with visual feedback.

--- src/components/items/world.item.tsx
+++ src/components/items/world.item.tsx
@@ -2,9 +2,10 @@ import { useQueryClient } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 import { formatDistanceToNow } from "date-fns";
-import { DownloadCloudIcon, PlayIcon } from "lucide-react";
+import { DownloadCloudIcon, PlayIcon, SproutIcon } from "lucide-react";
 import { useRef } from "react";
 import { toast } from "sonner";
+import { useCopyToClipboard } from "@/hooks/use-copy-to-clipboard";
 import { useDownloadVersion } from "@/hooks/use-download-version";
 import {
 	installedVersionsQueryKey,
@@ -12,6 +13,7 @@ import {
 } from "@/hooks/use-installed-versions";
 import type { GameData } from "@/hooks/use-saves";
 import type { ProgressPayload } from "@/lib/types";
+import { cn } from "@/lib/utils";
 import { useInstallations } from "@/stores/installations";
 import { Button } from "../ui/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
@@ -19,6 +21,7 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 export const WorldItem = ({ world }: { world: [GameData, string, string] }) => {
 	const { installations } = useInstallations();
 	const { data: versions } = useInstalledVersions();
+	const [copiedText, copyToClipboard] = useCopyToClipboard();
 	const worldData = world[0];
 	const installation = installations.find(
 		(installation) => installation.path.split("/").pop() === world[2],
@@ -80,7 +83,30 @@ export const WorldItem = ({ world }: { world: [GameData, string, string] }) => {
 		>
 			<div className="grid grid-cols-3 justify-between w-full items-center">
 				<div className="flex flex-col">
-					<p className="text-sm">{worldData.world_name}</p>
+					<p className="text-sm">
+						{worldData.world_name}
+						<Tooltip>
+							<TooltipTrigger
+								className={cn([
+									"ml-2 text-xs opacity-50 cursor-pointer",
+									worldData.seed.toString() === copiedText && "text-green-400",
+								])}
+								onClick={() => copyToClipboard(worldData.seed.toString())}
+							>
+								<SproutIcon className="inline h-4 w-4" />
+							</TooltipTrigger>
+							<TooltipContent className="flex flex-col gap-1 text-center">
+								Seed: {worldData.seed}
+								{worldData.seed.toString() === copiedText ? (
+									<span className="text-green-400">Copied!</span>
+								) : (
+									<span className="text-muted-foreground text-xs">
+										Click to copy
+									</span>
+								)}
+							</TooltipContent>
+						</Tooltip>
+					</p>
 					<p className="text-xs text-muted-foreground">
 						by{" "}
 						<span className="text-warning-foreground">
