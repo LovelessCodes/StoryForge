e1d8531f687852baa3a9ef601ca9f1ec121fd5e2
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-10-02 14:09:10 +0200

    feat(edit-world-dialog): ✨ add `identifier` to `updateWorld` call
    * Included `identifier` from `world[0].savegame_identifier` in the `updateWorld` function call to ensure proper identification during world updates.
    * This change enhances the functionality of the `EditWorldDialog` component.

--- src/components/items/world.item.tsx
+++ src/components/items/world.item.tsx
@@ -6,8 +6,8 @@ import {
 	DownloadCloudIcon,
 	PlayIcon,
 	SproutIcon,
+	TrashIcon,
 	WrenchIcon,
-	XIcon,
 } from "lucide-react";
 import { useRef } from "react";
 import { toast } from "sonner";
@@ -231,7 +231,11 @@ export const WorldItem = ({ world }: { world: [GameData, string, string] }) => {
 								size="icon"
 								variant="outline"
 							>
-								<XIcon aria-hidden="true" className="opacity-60" size={16} />
+								<TrashIcon
+									aria-hidden="true"
+									className="opacity-60"
+									size={16}
+								/>
 							</Button>
 						</TooltipTrigger>
 						<TooltipContent>Delete</TooltipContent>
