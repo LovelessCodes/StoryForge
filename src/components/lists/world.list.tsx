70b6b237ec4bbf6b32a30e88ae10dcced66adef6
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-10-03 11:15:51 +0200

    feat(world-list): ✨ add animation support for world items
    * Wrapped world items in `AnimatePresence` for improved motion animations.
    * Enhanced user experience by providing visual feedback during list updates.

--- src/components/lists/world.list.tsx
+++ src/components/lists/world.list.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence } from "motion/react";
 import type { GameData } from "@/hooks/use-saves";
 import { WorldItem } from "../items/world.item";
 
@@ -8,19 +9,25 @@ export function WorldList({
 }) {
 	return (
 		<div className="flex flex-col overflow-y-auto h-full relative">
-			{worlds
-				.sort((a, b) => {
-					const aLastPlayed = a[0].last_played
-						? new Date(a[0].last_played).getTime()
-						: 0;
-					const bLastPlayed = b[0].last_played
-						? new Date(b[0].last_played).getTime()
-						: 0;
-					return bLastPlayed - aLastPlayed;
-				})
-				.map((world) => (
-					<WorldItem key={world[0].world_name + world[1]} world={world} />
-				))}
+			<AnimatePresence>
+				{[...worlds]
+					.sort((a, b) => {
+						const aLastPlayed = a[0].last_played
+							? new Date(a[0].last_played).getTime()
+							: 0;
+						const bLastPlayed = b[0].last_played
+							? new Date(b[0].last_played).getTime()
+							: 0;
+						return bLastPlayed - aLastPlayed;
+					})
+					.map((world, i) => (
+						<WorldItem
+							index={i}
+							key={world[0].world_name + world[1]}
+							world={world}
+						/>
+					))}
+			</AnimatePresence>
 		</div>
 	);
 }
