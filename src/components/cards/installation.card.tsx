628ae656086b8c912882105ecb9d924cf15f4b20
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-10-09 10:48:08 +0200

    refactor(installation.card): 🔧 simplify `useDownloadVersion` implementation
    * Removed error handling and progress listening logic from `useDownloadVersion`.
    * Streamlined the component by focusing on essential functionality.

--- src/components/cards/installation.card.tsx
+++ src/components/cards/installation.card.tsx
@@ -1,5 +1,3 @@
-import { useQueryClient } from "@tanstack/react-query";
-import { listen } from "@tauri-apps/api/event";
 import { formatDistanceToNow } from "date-fns";
 import {
 	DownloadCloudIcon,
@@ -8,15 +6,9 @@ import {
 	Play,
 	Star,
 } from "lucide-react";
-import { useRef } from "react";
-import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { useDownloadVersion } from "@/hooks/use-download-version";
-import {
-	installedVersionsQueryKey,
-	useInstalledVersions,
-} from "@/hooks/use-installed-versions";
-import type { ProgressPayload } from "@/lib/types";
+import { useInstalledVersions } from "@/hooks/use-installed-versions";
 import { cn } from "@/lib/utils";
 import type { Installation } from "@/stores/installations";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
@@ -36,55 +28,8 @@ export function InstallationCard({
 	onEdit,
 	onAddMods,
 }: InstallationCardProps) {
-	const listenRef = useRef<() => void>(null);
-	const queryClient = useQueryClient();
-
 	const { mutate: installVersion, isPending: isInstalling } =
-		useDownloadVersion({
-			onError: (error, v) => {
-				listenRef.current?.();
-				toast.error(`Error downloading game version: ${error.message}`, {
-					id: `download-game-version-${v}`,
-				});
-			},
-			onMutate: async (v) => {
-				toast.loading(`Starting to download game version ${v}...`, {
-					id: `download-game-version-${v}`,
-				});
-				listenRef.current = await listen<ProgressPayload>(
-					`download://version:${v.replace(/\./g, "_")}`,
-					(event) => {
-						const { phase, percent } = event.payload;
-						if (phase === "download") {
-							toast.loading(
-								`Downloading game version ${v}: ${percent?.toFixed(0)}%`,
-								{
-									id: `download-game-version-${v}`,
-								},
-							);
-						}
-						if (phase === "extract") {
-							toast.loading(`Extracting game version ${v}`, {
-								id: `download-game-version-${v}`,
-							});
-						}
-					},
-				);
-			},
-			onSuccess: (d, v) => {
-				listenRef.current?.();
-				if (d === "already_downloaded") {
-					toast.dismiss(`download-game-version-${v}`);
-					return;
-				}
-				toast.success(`Game version ${v} downloaded`, {
-					id: `download-game-version-${v}`,
-				});
-				queryClient.invalidateQueries({
-					queryKey: installedVersionsQueryKey(),
-				});
-			},
-		});
+		useDownloadVersion();
 
 	const { data: versions } = useInstalledVersions();
 	return (
