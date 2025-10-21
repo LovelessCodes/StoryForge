e836488c971e0fa9c2236f23828810f19ca45595
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-10-21 10:36:33 +0200

    refactor(worlds): 🔄 simplify `WorldItem` component and improve structure
    * Removed unused `motion` animations for better performance.
    * Streamlined props by eliminating `index` and adjusting the component structure.
    * Enhanced readability and maintainability of the code.

--- src/components/lists/world.list.tsx
+++ src/components/lists/world.list.tsx
@@ -1,12 +1,14 @@
 import { AnimatePresence } from "motion/react";
 import { WorldItem } from "@/components/items/world.item";
 import type { World } from "@/lib/types";
+import { itemVariants } from "@/lib/utils";
+import { MotionWorldContextMenu } from "../context-menus/world.context-menu";
 
 export function WorldList({ worlds }: { worlds: World[] }) {
 	return (
 		<div className="flex flex-col w-full bg-card p-2 rounded shadow border relative overflow-y-auto">
 			<AnimatePresence>
-				{[...worlds]
+				{worlds
 					.sort((a, b) => {
 						const aLastPlayed = a.data.last_played
 							? new Date(a.data.last_played).getTime()
@@ -16,12 +18,23 @@ export function WorldList({ worlds }: { worlds: World[] }) {
 							: 0;
 						return bLastPlayed - aLastPlayed;
 					})
-					.map((world, i) => (
-						<WorldItem
-							index={i}
+					.map((world, index) => (
+						<MotionWorldContextMenu
+							animate="show"
+							className="not-last:border-b p-2 flex gap-2 w-full"
+							custom={index}
+							exit="exit"
+							initial="hidden"
 							key={world.data.world_name + world.installation_name}
+							layout="position"
+							variants={itemVariants}
 							world={world}
-						/>
+						>
+							<WorldItem
+								key={world.data.world_name + world.installation_name}
+								world={world}
+							/>
+						</MotionWorldContextMenu>
 					))}
 				{worlds.length === 0 && (
 					<p className="p-4 text-sm text-muted-foreground select-none">
