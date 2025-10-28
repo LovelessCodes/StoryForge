932b9771798dafe0e944b7f878311c0a0ede16d0
Mike Kold Hermann <mkh@cleanmanager.dk>
2025-10-28 10:37:07 +0100

    refactor(dialog.addversion): ✨ update `TooltipTrigger` usage for improved structure
    * Refactored the `TooltipTrigger` component to use the `render` prop for better readability and maintainability.
    * Enhanced the structure of the label within the tooltip for clearer error handling.

--- src/components/dialogs/addversion.dialog.tsx
+++ src/components/dialogs/addversion.dialog.tsx
@@ -104,19 +104,21 @@ export function AddVersionDialog({ open }: { open: boolean }) {
 							{(field) => (
 								<div className="grid gap-2">
 									<Tooltip>
-										<TooltipTrigger asChild>
-											<Label
-												className={clsx([
-													field.state.meta.errors.length
-														? "text-destructive"
-														: "",
-													"w-fit",
-												])}
-												htmlFor="version"
-											>
-												Version
-												<span className="text-destructive">*</span>
-											</Label>
+										<TooltipTrigger
+											render={
+												<Label
+													className={clsx([
+														field.state.meta.errors.length
+															? "text-destructive"
+															: "",
+														"w-fit",
+													])}
+													htmlFor="version"
+												/>
+											}
+										>
+											Version
+											<span className="text-destructive">*</span>
 										</TooltipTrigger>
 										<TooltipContent align="start" side="bottom">
 											<p className="text-xs">Pick game version</p>
