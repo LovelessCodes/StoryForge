5b47466ef64ef1c139db534d035b4d16263d1954
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-05-28 17:20:49 +0200

    feat: add window controls for macOS and Windows

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,16 +5,22 @@ import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { platform } from "@tauri-apps/plugin-os";
 import { relaunch } from "@tauri-apps/plugin-process";
 import * as React from "react";
 import { Toaster, toast } from "sonner";
 
+import { MaximizeIcon } from "@/components/icons/maximize";
+import { MinimizeIcon } from "@/components/icons/minimize";
+import { XIcon } from "@/components/icons/x";
 import { AppSidebar } from "@/components/sidebars/app.sidebar";
 import {
   AlertDialog,
   AlertDialogContent,
   AlertDialogCreateHandle,
 } from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 import { CommandCreateHandle, CommandDialog, CommandDialogPopup } from "@/components/ui/command";
 import { Dialog, DialogContent, DialogCreateHandle } from "@/components/ui/dialog";
 import { Drawer, DrawerContent, DrawerCreateHandle } from "@/components/ui/drawer";
@@ -99,10 +105,70 @@ function RootComponent() {
   return (
     <React.Fragment>
       <Sidebar variant="inset" collapsible="icon">
+        {["macos", "windows"].includes(platform()) && (
+          <div className="flex w-screen justify-between" data-tauri-drag-region>
+            <div className="flex items-center gap-2 p-1" data-tauri-drag-region>
+              <SidebarTrigger />
+              {platform() === "macos" && (
+                <>
+                  <div
+                    onClick={() => getCurrentWindow().close()}
+                    className="group/close flex size-3 items-center justify-center rounded-full bg-red-500 text-white transition-colors hover:bg-red-700 dark:bg-red-400 hover:dark:bg-red-600"
+                  >
+                    <XIcon
+                      className="opacity-0 transition-opacity group-hover/close:opacity-100"
+                      size={10}
+                    />
+                  </div>
+                  <div
+                    onClick={() => getCurrentWindow().minimize()}
+                    className="group/minimize flex size-3 items-center justify-center rounded-full bg-yellow-500 text-white transition-colors hover:bg-yellow-700 dark:bg-yellow-300 hover:dark:bg-yellow-600"
+                  >
+                    <MinimizeIcon
+                      className="opacity-0 transition-opacity group-hover/minimize:opacity-100"
+                      size={10}
+                    />
+                  </div>
+                  <div
+                    onClick={() => getCurrentWindow().toggleMaximize()}
+                    className="group/maximize flex size-3 items-center justify-center rounded-full bg-green-500 text-white transition-colors hover:bg-green-700 dark:bg-green-500 hover:dark:bg-green-600"
+                  >
+                    <MaximizeIcon
+                      className="opacity-0 transition-opacity group-hover/maximize:opacity-100"
+                      size={8}
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="flex w-full items-center justify-end gap-2 px-3" data-tauri-drag-region>
+              {platform() === "windows" && (
+                <>
+                  <Button
+                    onClick={() => getCurrentWindow().minimize()}
+                    size="icon-sm"
+                    variant="ghost"
+                  >
+                    <MinimizeIcon className="hover:text-warning transition-colors" size={16} />
+                  </Button>
+                  <Button
+                    onClick={() => getCurrentWindow().toggleMaximize()}
+                    size="icon-sm"
+                    variant="ghost"
+                  >
+                    <MaximizeIcon className="hover:text-success transition-colors" size={16} />
+                  </Button>
+                  <Button onClick={() => getCurrentWindow().close()} size="icon-sm" variant="ghost">
+                    <XIcon className="hover:text-destructive transition-colors" size={16} />
+                  </Button>
+                </>
+              )}
+            </div>
+          </div>
+        )}
         <AppSidebar />
       </Sidebar>
       <SidebarInset>
-        <SidebarTrigger className="absolute top-1 left-1" />
         <Outlet />
       </SidebarInset>
       <TanStackDevtools
@@ -116,6 +182,13 @@ function RootComponent() {
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
+        config={{
+          hideUntilHover: true,
+          position: "top-right",
+          customTrigger: () => (
+            <span className="opacity-0 transition-opacity hover:opacity-100">Devtools</span>
+          ),
+        }}
       />
       <Tooltip handle={rootTooltipHandle}>
         {({ payload: Payload }) => <TooltipContent>{Payload && <Payload />}</TooltipContent>}
