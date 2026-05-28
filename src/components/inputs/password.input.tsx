a2de88fc1aac9c97c1dffad24c327ed2e6cba0c6
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-05-28 17:00:10 +0200

    feat: refactor password input to use input group and button components

--- src/components/inputs/password.input.tsx
+++ src/components/inputs/password.input.tsx
@@ -1,14 +1,14 @@
-import { clsx } from "clsx";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
-import { forwardRef, useId, useState } from "react";
+import { useId, useState } from "react";
 
-import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
 
 type PasswordInputProps = {
   className?: string | ((active: boolean) => string);
 } & Omit<React.InputHTMLAttributes<HTMLInputElement>, "className">;
 
-const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(({ className, ...rest }) => {
+export function PasswordInput({ className, ...rest }: PasswordInputProps) {
   const id = useId();
   const [visible, setVisible] = useState<boolean>(false);
 
@@ -19,33 +19,22 @@ const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(({ classN
   const computedClassName = typeof className === "function" ? className(visible) : className;
 
   return (
-    <div className="relative">
-      <Input
-        className={clsx(["pe-9", computedClassName])}
+    <InputGroup>
+      <InputGroupInput
         id={id}
-        placeholder={visible ? "p4zzw0rd" : "········"}
         type={visible ? "text" : "password"}
-        {...rest}
+        placeholder="········"
+        value={rest.value}
+        onChange={rest.onChange}
+        className={computedClassName}
       />
-      <button
-        aria-controls="password"
-        aria-label={visible ? "Hide password" : "Show password"}
-        aria-pressed={visible}
-        className="text-muted-foreground/80 hover:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-md transition-[color,box-shadow] outline-none focus:z-10 focus-visible:ring-[3px] disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
-        onClick={toggleVisibility}
-        tabIndex={-1}
-        type="button"
-      >
-        {visible ? (
-          <EyeOffIcon aria-hidden="true" size={16} />
-        ) : (
-          <EyeIcon aria-hidden="true" size={16} />
-        )}
-      </button>
-    </div>
+      <InputGroupAddon align="inline-end">
+        <Button variant="ghost" onClick={toggleVisibility}>
+          {visible ? <EyeIcon size={16} /> : <EyeOffIcon size={16} />}
+        </Button>
+      </InputGroupAddon>
+    </InputGroup>
   );
-});
+}
 
 PasswordInput.displayName = "PasswordInput";
-
-export default PasswordInput;
