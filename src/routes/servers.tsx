27dd16eed9c866a0fa416e8a0d7c7b035cf3ac35
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-05-28 16:59:13 +0200

    feat: adjust padding for mobile view in servers route

--- src/routes/servers.tsx
+++ src/routes/servers.tsx
@@ -25,7 +25,7 @@ function RouteComponent() {
 
   return (
     <div className="grid h-full w-full grid-rows-[min-content_auto]">
-      <div className="flex h-fit pt-1 pr-2 pl-9">
+      <div className="flex h-fit pt-1 pr-2 pl-2 max-md:pl-9">
         <Button
           className="w-full cursor-pointer justify-between"
           render={<DialogTrigger handle={rootDialogHandle} payload={() => <AddServerDialog />} />}
