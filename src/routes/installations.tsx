55cffde04199e47bc508710555278e72cc188f9c
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-05-28 16:59:36 +0200

    feat: adjust padding for mobile view in installations route

--- src/routes/installations.tsx
+++ src/routes/installations.tsx
@@ -27,7 +27,7 @@ function RouteComponent() {
 
   return (
     <div className="grid w-full grid-rows-[min-content_auto] gap-2">
-      <div className="grid h-fit grid-cols-[auto_min-content] gap-2 pt-1 pr-2 pl-9">
+      <div className="grid h-fit grid-cols-[auto_min-content] gap-2 pt-1 pr-2 pl-2 max-md:pl-9">
         <Button
           className="w-full cursor-pointer justify-between"
           render={
