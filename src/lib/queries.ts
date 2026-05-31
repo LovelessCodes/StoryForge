689057646b420434db5868127dd15a7df662ce03
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-05-31 21:18:45 +0200

    feat: add staleTime to queries
    Set staleTime to Infinity for modTagsQuery and gameVersionsQuery to
    prevent unnecessary refetches

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -5,9 +5,11 @@ import type { ModTag } from "./types";
 export const modTagsQuery = {
   queryFn: () => invoke("fetch_mod_tags") as Promise<ModTag[]>,
   queryKey: ["modTags"],
+  staleTime: Infinity,
 };
 
 export const gameVersionsQuery = {
   queryFn: () => invoke("fetch_versions") as Promise<string[]>,
   queryKey: ["gameVersions"],
+  staleTime: Infinity,
 };
