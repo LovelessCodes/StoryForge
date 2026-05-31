afd965e23bfb575dc2551de925d69b8bb2f59233
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-05-31 21:36:53 +0200

    feat: add mods button component

---
+++ src/components/sidebars/buttons/mods.button.tsx
@@ -0,0 +1,16 @@
+import { useLoaderData } from "@tanstack/react-router";
+import { PackagePlusIcon } from "lucide-react";
+
+import { SidebarMenuSubItem, SidebarMenuSubButton } from "@/components/ui/sidebar";
+
+export function ModsButton() {
+  const { installation } = useLoaderData({ from: "/install-mods/$id" });
+  return (
+    <SidebarMenuSubItem>
+      <SidebarMenuSubButton size="sm" isActive>
+        <PackagePlusIcon />
+        {installation.name}
+      </SidebarMenuSubButton>
+    </SidebarMenuSubItem>
+  );
+}
