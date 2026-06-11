234b892c3492b391b9d3ff025a892432a489041c
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-06-11 17:06:53 +0200

    Remove server hosting detail route

--- src/routes/server-hosting/$id.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { ServerHostingDetailPage } from "@/components/pages/server-hosting-detail";
-import { ErrorComponent } from "@/components/ui/error";
-
-export const Route = createFileRoute("/server-hosting/$id")({
-  component: ServerHostingDetailPage,
-  errorComponent: ErrorComponent,
-});
