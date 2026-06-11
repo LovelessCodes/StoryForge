653f9156c705ab7a614009942bfb72723b66eb36
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-06-11 17:08:57 +0200

    feat: update import path for OutputMod type
    Update import path from `components/pages/install-mods` to
    `components/pages/mods-browser`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ import { type ClassValue, clsx } from "clsx";
 import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 
-import type { OutputMod } from "@/components/pages/install-mods";
+import type { OutputMod } from "@/components/pages/mods-browser";
 import type { Installation } from "@/stores/installations";
 
 export function cn(...inputs: ClassValue[]) {
